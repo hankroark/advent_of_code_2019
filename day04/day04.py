e2deea10dcf3bdfdc4102d31b1b72eb8a26e7f3f
Hank Roark <hank.roark@gmail.com>
2019-12-05 14:20:48 -0800

    Day 4 challenge, Part 1 and 2

--- day04/day04.py
+++ day04/day04.py
@@ -1,3 +1,4 @@
+from collections import Counter
 
 """
 You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password
@@ -17,27 +18,29 @@ Other than the range rule, the following are true:
 How many different passwords within the range given in your puzzle input meet these criteria?
 """
 
-def is_valid(password: int) -> bool:
+def is_valid(password: int, exact_pair=False) -> bool:
     password_s = str(password)
     if len(password_s) != 6:
         return False
 
     password_i = [int(s) for s in password_s]
     compare = password_i[0]
-    found_pair = False
     for _, val in enumerate(password_i[1:]):
         if val < compare:
             return False
-        else:
-            if val == compare:
-                found_pair = True
-            compare = val
+        compare = val
+
+    counts = Counter(password_s)
+    if not exact_pair:
+        found_pair = max(counts.values()) > 1
+    else:
+        found_pair = 2 in counts.values()
 
     return found_pair
 
 
 assert not is_valid(123)
-assert is_valid(111111)
+assert is_valid(111111)   # ok for part 1
 assert not is_valid(223450)
 assert not is_valid(123789)
 
@@ -46,3 +49,22 @@ valid_words = [is_valid(password) for password in range(240298, 784956+1)]
 print(sum(valid_words))  # 1150 for Part 1
 
 
+"""
+--- Part Two ---
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of 
+matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+How many different passwords within the range given in your puzzle input meet all of the criteria?
+"""
+assert not is_valid(111111, exact_pair=True) # part 2 rules
+assert is_valid(112233, exact_pair=True)
+assert not is_valid(123444, exact_pair=True)
+assert is_valid(111122, exact_pair=True)
+
+valid_words = [is_valid(password, exact_pair=True) for password in range(240298, 784956+1)]
+print(sum(valid_words))  # 748 for Part 2
