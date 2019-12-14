a043f8b8ecb329a8a5accdf22492a187e59e45ad
Hank Roark <hank.roark@gmail.com>
2019-12-14 12:34:54 -0800

    Removed text that shouldn't have been there to begin with.

--- src/day03/day03.py
+++ src/day03/day03.py
@@ -6,39 +6,6 @@ Point = Tuple[int, int]
 Locations = List[Point]
 
 """
-The wires twist and turn, but the two wires occasionally cross paths. To fix the circuit, you need to find the 
-intersection point closest to the central port. Because the wires are on a grid, use the Manhattan distance for this 
-measurement. While the wires do technically cross right at the central port where they both start, this point does not 
-count, nor does a wire count as crossing with itself.
-
-For example, if the first wire's path is R8,U5,L5,D3, then starting from the central port (o), it goes right 8, up 5, 
-left 5, and finally down 3:
-
-...........
-...........
-...........
-....+----+.
-....|....|.
-....|....|.
-....|....|.
-.........|.
-.o-------+.
-...........
-Then, if the second wire's path is U7,R6,D4,L4, it goes up 7, right 6, down 4, and left 4:
-
-...........
-.+-----+...
-.|.....|...
-.|..+--X-+.
-.|..|..|.|.
-.|.-X--+.|.
-.|..|....|.
-.|.......|.
-.o-------+.
-...........
-These wires cross at two locations (marked X), but the lower-left one is closer to the central port: its distance is 
-3 + 3 = 6.
-
 Here are a few more examples:
 
 R75,D30,R83,U83,L12,D49,R71,U7,L72
